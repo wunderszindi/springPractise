5c0f3459d4594b90f6deecda5336ddc76373f8d7
wunderszindi <sandrine.varga@gmail.com>
2019-05-02 14:51:56 +0200

    initial commit

---
+++ shoppinglist/src/test/java/hu/flowacademy/shoppinglist/ShoppinglistApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.flowacademy.shoppinglist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShoppinglistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
